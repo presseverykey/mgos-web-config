4f557cb2176aa783ee1af01358dde8d5de2e3e2e
Tim Becker <tim@presseverykey.com>
2019-10-03 19:18:38 +0200

    config based pwm

--- src/main.c
+++ src/main.c
@@ -4,9 +4,10 @@
 
 enum mgos_app_init_result mgos_app_init(void) {
   LOG(LL_INFO, ("PWM demo starting..."));
-  int pin = 13;
-  int freq = 550;
-  float duty = (float)50 / 100.0;
+  int pin = mgos_sys_config_get_ws_pwm_pin();
+  int freq = mgos_sys_config_get_ws_pwm_freq();
+  int duty_i = mgos_sys_config_get_ws_pwm_duty();
+  float duty = (float)duty_i / 100.0;
   
   mgos_pwm_set(pin, freq, duty);
   return MGOS_APP_INIT_SUCCESS;
